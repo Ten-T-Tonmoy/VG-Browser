be52dce29b225c35ff5b83100984df14435582d2
Ten-T-Tonmoy <stu01754@gmail.com>
2025-01-09 02:04:12 +0600

    made layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
+  //show hides ? layout making
   return (
-    <div>
-      <Button colorScheme="teal" mt={4} >
-        Click Me
-      </Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "side main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        {" "}
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="side" bg="red.400">
+          {" "}
+          side
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
