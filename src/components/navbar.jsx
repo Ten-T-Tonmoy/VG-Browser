1088eda6e1d3d27b399770313d6d7144c8831852
Ten-T-Tonmoy <stu01754@gmail.com>
2025-01-21 23:59:04 +0600

    fixed navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,9 +2,18 @@ import logo from "../assets/logo.webp";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row items-center">
-      <img src={logo} alt="Logo" className="w-[50px] h-[50px]" />
-      <p className="text-red-500">Topbar Beatch</p>
+    <div
+      className="flex flex-row 
+      fixed top-0 left-0 w-full  items-center
+      backdrop-blur-md bg-white/30 border-b border-white/20 shadow-lg"
+    >
+      <img src={logo} alt="Logo" className="w-[60px] h-[60px]" />
+      <div
+        className="bg-gradient-to-r bg-clip-text text-transparent from-red-500
+       to-violet-500  text-[1.5rem] font-bold text-center flex-1 lg:ml-0 "
+      >
+        VidGi Library
+      </div>
     </div>
   );
 };
