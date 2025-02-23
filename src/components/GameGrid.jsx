631300940b2998b640d8590d9fed5dc4186ce4a5
Ten-T-Tonmoy <stu01754@gmail.com>
2025-02-23 23:56:26 +0600

    skeleton painful shii

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -2,13 +2,16 @@ import useGames from "../hooks/useGames";
 import Gcards from "./Gcards";
 import PlatSelect from "./PlatSelect";
 import { useState } from "react";
+import GcardSkeleton from "./GcardSkeleton";
 
 //shits u need to do
 //filter by genre then by platform since common(he using to change the url)
 
 const GameGrid = () => {
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   const [selectplat, setSelectPlat] = useState(null);
-  const { games, err } = useGames();
+  const { games, err, loading } = useGames();
   console.log(games);
   const filteredGames = selectplat
     ? games.filter((game) => game.platform === selectplat.id)
@@ -22,6 +25,7 @@ const GameGrid = () => {
         className="grid mt-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 
       gap-4 justify-items-center m-2"
       >
+        {loading && skeletons.map((skel) => <GcardSkeleton key={skel} />)}
         {filteredGames?.map((game) => (
           <Gcards key={game.id} game={game} />
         ))}
---
+++ src/components/GcardSkeleton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const GcardSkeleton = () => {
+  return (
+    <div
+      className=" w-full h-[400px] bg-gray-100 cursor-pointer
+  hover:bg-gray-300 ease-in-out duration-200 dark:hover:bg-gray-700  dark:bg-gray-800 shadow-lg
+rounded-md lg:max-w-[320px] "
+    >
+      <img className="rounded-md " alt="" />
+      <div className="flex mx-3 justify-between">Scores and icons </div>
+      <div>
+        <div
+          className="flex font-mono my-2 font-bold 
+    text-[30px] dark:text-white text-center justify-center"
+        >
+          Game Name
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GcardSkeleton;
